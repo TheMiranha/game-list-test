e305cd175af51fa5a038b9a49d71961d91c2abef
TheMiranha <lxbrmiranda@gmail.com>
2023-06-23 00:20:17 -0300

    fix: remove bff mock

--- src/modules/api/games/application/index.ts
+++ src/modules/api/games/application/index.ts
@@ -5,11 +5,10 @@ import { GamesUseCase } from './games.usecase'
 
 const gamesRepository = new GamesTestRepository()
 const fakeGamesRepository = new GamesFakeRepository()
-// const gamesUseCase = new GamesUseCase(
-//   process.env.NODE_ENV == 'test' ? fakeGamesRepository : gamesRepository
-// )
+const gamesUseCase = new GamesUseCase(
+  process.env.NODE_ENV == 'test' ? fakeGamesRepository : gamesRepository
+)
 
-const gamesUseCase = new GamesUseCase(fakeGamesRepository)
 const gamesController = new GamesController(gamesUseCase)
 
 export { gamesController }
