968fa4e76384bcdefc65d4218a62304d56f0d680
TheMiranha <lxbrmiranda@gmail.com>
2023-07-14 23:26:23 -0300

    fix: rates

--- src/pages/api/rates.ts
+++ src/pages/api/rates.ts
@@ -2,7 +2,7 @@ import { ratesController } from '@/modules/api/rates/application'
 import { IResponse } from '@/modules/api/rates/domain/_types/route'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-export default function handler(
+export async function POST(
   req: NextApiRequest,
   res: NextApiResponse<IResponse>
 ) {
